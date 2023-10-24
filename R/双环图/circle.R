082f2812e19d90825b13fead4109588f65f373c1
Aning-Q <Aning_kqq@163.com>
2023-10-25 00:03:51 +0800

    可以根据输入表格列的顺序输出图片

--- R/双环图/circle.R
+++ R/双环图/circle.R
@@ -1,8 +1,8 @@
 library(lmbio)
 library(ggplot2) # 绘图
-sheet <- getsheetname("Molecular_weight.xlsx")
-k <- 2
-data <- readdata("Molecular_weight.xlsx",k)
+sheet <- getsheetname("circle.xlsx")
+k <- 1
+data <- readdata("circle.xlsx",k)
 df <- data
 name <- colnames(data)[-1]
 for (i in 1:length(name)) {
@@ -21,30 +21,29 @@ for (i in 1:length(name)) {
   #df <- merge(df, df1[,c(1,3,4,5)], by.x = 'range', by.y = 'range')
   df <- cbind(df, df1[,c(3,4,5)])
 }
-df$x <- name[1]
-df$y <- name[2]
-df$z <- "A"
-df$range <- factor(df$range, levels=df$range)
+
+dd <- rbind(
+      #data.frame(range=df$range,group="A",per=1,pernum=1,label="A"),
+      data.frame(range=df$range,group=2,per=df$per1,pernum = df$per.y1,label=df$label1),
+      data.frame(range=df$range,group=3,per=df$per2,pernum=df$per.y2,label=df$label2))
+
+dd$range <- factor(dd$range, levels=unique(dd$range))
+#dd$group <- factor(dd$group,levels = c('A',name[1],name[2]))
 col1 <- c("#7f96ff","#f2b5d4","#3dccc7","#e4c1f9","#ffa69e","#ffd6a5")
 col <- c("#EEBD6C","#D94E32","#834127","#EDDDC2","#4DAB90","#8EB79E")
-p<- ggplot(df) +
-  geom_bar(aes(z, per2), 
+p<- ggplot(dd) +
+  geom_bar(aes("1", 1/nrow(dd)), 
            stat = 'identity', width = .8, color = 'white',fill = 'white') +
   geom_text(aes(0.65, 0.65, 
                 label = paste0("内圈:",name[1],"\n\n","外圈:",name[2])),
             size =5, color = 'black')+
-  geom_bar(aes(x, 
-               per1, 
+  geom_bar(aes(group, 
+               per, 
                fill = range),
            stat = 'identity', width = 0.6,color = 'white')+
-  geom_text(aes(2, as.numeric(per.y1), 
-                label = label1),
-            size =3, color = 'black')+
-  geom_bar(aes(y, per2, fill = range), 
-           stat = 'identity', width = 0.6, color = 'white') +
-  # 添加标签
-  geom_text(aes(3, as.numeric(per.y2),label = label2),
-            size = 3, color = 'black') +
+  geom_text(aes(group, as.numeric(pernum), 
+                label = label),
+            size =3, color = 'black') +
   
   scale_y_continuous(labels = scales::percent) +
   coord_polar(theta = "y") + # 转换坐标轴
@@ -54,7 +53,7 @@ p<- ggplot(df) +
   theme(plot.margin = unit(c(-0.5, 1, -0.5, -2), "cm"),
         legend.title = element_blank(),
         legend.text = element_text(size = 10))
-savexlsx(df[c("range",name[1],name[2],"per1","label1","per2","label2")],paste0("Circle_",sheet[k],".xlsx"))
+savexlsx(df[c("range",name[1],name[2],"per1","per2")],paste0("Circle_",sheet[k],".xlsx"))
 ggplotsave(p,  
            mapname = paste0("Circle_",sheet[k]),  
            imagetype = c("png","pdf"),  
